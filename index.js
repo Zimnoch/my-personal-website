4845d2ed08b63129634fe19926c3f47cf32145f6
Amanda Zimnoch <zimnoch.2@gmail.com>
2022-09-05 15:36:31 -0400

    changed theme to otgw

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Ain't That Just the Way";
 
 document.querySelector("body").appendChild(h2);
